b0028dd8c2cda2bb6989405f691dde6ce18ebce6
sunil pinto <sunil.pinto@oracle.com>
2022-03-14 11:25:30 -0700

    rs

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func newPool(write bool) *redis.Pool {
 		// Maximum number of idle connections in the pool.
 		MaxIdle: 80,
 		// max number of connections
-		MaxActive: 12000,
+		MaxActive: 11000,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", redisHost+":"+redisPort)
 			if err != nil {
